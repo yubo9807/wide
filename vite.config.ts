b618a10c555877353de6ac88c805eec268acd1fa
yangyb <yangyb@boyachain.cn>
2023-02-27 14:53:33 +0800

    feat: gzip 压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
+import viteCompression from 'vite-plugin-compression';
 declare const __dirname: string;
 
 const proxy = {
@@ -16,7 +17,12 @@ const proxy = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      threshold: 1024 * 300,
+    })
+  ],
   resolve: {
     alias: {
       // 键必须以斜线开始和结束
@@ -49,10 +55,12 @@ export default defineConfig({
       output: {
         manualChunks(url) { // 分包
           if (url.includes('node_modules')) {
-            if (url.includes('@vue')) return 'vue';
+            if (url.includes('@vue')) return 'vue-'+Math.trunc(Date.now()/1000);
             if (url.includes('vue-router')) return 'vue-router';
             if (/tslib|dayjs|lodash|memoize/.test(url)) return 'tools';
-            return url.toString().split('node_modules/')[1].split('/')[0].toString();
+            if (/element-plus/.test(url)) return 'element-plus';
+            if (/echarts/.test(url)) return 'echarts';
+            // return url.toString().split('node_modules/')[1].split('/')[0].toString();
           }
           if (url.includes('src/common/utils')) return 'utils';
           if (url.includes('src/sub-admin/layout')) return 'layout';
